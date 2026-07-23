992caeee64202fe6e7189f0f2578405cdb5b56f4
COREO CX <coreo-cx@local>
2026-07-24 01:56:44 +0800

    Replace Stage 01 animation assets with background-removed videos

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const COREZAX_CACHE = 'corezax-v0109-stage01-animation-test-20260724';
+const COREZAX_CACHE = 'corezax-v0109-stage01-animation-nobg-20260724';
 const COREZAX_ASSETS = [
   './',
   './index.html',
